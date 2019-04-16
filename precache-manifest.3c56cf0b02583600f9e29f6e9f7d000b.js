341e86d60ba43f89f2a74d92334cf0200c846aac
Anton Kalmykov <a.kalmykov@mobidev.biz>
2019-04-16 18:01:12 +0300

    Updates

--- precache-manifest.3c56cf0b02583600f9e29f6e9f7d000b.js
+++ precache-manifest.3c56cf0b02583600f9e29f6e9f7d000b.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/next-photo/static/js/runtime~main.3ecb45c7.js"
   },
   {
-    "revision": "915360aab9dcb2ba2a71",
-    "url": "/next-photo/static/js/main.37763059.chunk.js"
+    "revision": "e9a95aaaf15c1ea23fe3",
+    "url": "/next-photo/static/js/main.e8485a79.chunk.js"
   },
   {
     "revision": "52819cec33f845d2f3f8",
     "url": "/next-photo/static/js/2.f448a5d0.chunk.js"
   },
   {
-    "revision": "915360aab9dcb2ba2a71",
+    "revision": "e9a95aaaf15c1ea23fe3",
     "url": "/next-photo/static/css/main.7324fec9.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/next-photo/static/css/2.b30a0aeb.chunk.css"
   },
   {
-    "revision": "cd68c6f4deeb487a74e5b8cf4c4f2297",
+    "revision": "a4bb41b1b1b26f7acc2be501b6d1b034",
     "url": "/next-photo/index.html"
   }
 ];
